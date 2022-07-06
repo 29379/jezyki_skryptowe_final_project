0f20bcf5ef037872c88054f59e430440d6d72a38
Kuba <100722793+29379@users.noreply.github.com>
2022-07-07 01:28:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,5 +11,3 @@ import os, sys
 if __name__ == '__main__':
     os.chdir("/home/Kuba/Desktop/tmp/jezyki_skryptowe_final_project/webapp/")
     os.system("python manage.py runserver")
-    
->>>>>>> 50c506f4ed61a07d48c8f6f629c011731fb9b376
